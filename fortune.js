850421ed45d82007d8ef93208a8e584d66d7aed6
Mabel00101 <3077533421@qq.com>
2026-07-26 13:43:15 +0800

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -1,73 +1,73 @@
 const fortunes = [
 
 {
+icon:"🐉",
 title:"乾卦·飞龙在天",
 level:"一等奖",
 reward:"888积分",
-desc:"事业腾飞，贵人相助",
-icon:"🐉"
+desc:"事业腾飞，贵人相助"
 },
 
 {
+icon:"🌏",
 title:"坤卦·厚德载物",
 level:"二等奖",
 reward:"188积分",
-desc:"积累福气，财运渐旺",
-icon:"🌏"
+desc:"福气积累，财运渐旺"
 },
 
 {
+icon:"🔥",
 title:"离卦·光明之象",
 level:"三等奖",
 reward:"88积分",
-desc:"灵感涌现，好事发生",
-icon:"🔥"
+desc:"灵感出现，好事临门"
 },
 
 {
+icon:"⚡",
 title:"震卦·雷动九天",
 level:"幸运奖",
 reward:"18积分",
-desc:"行动有惊喜",
-icon:"⚡"
+desc:"行动带来惊喜"
 },
 
 {
-title:"普通签",
+icon:"🍀",
+title:"平安签",
 level:"谢谢参与",
 reward:"福气+1",
-desc:"今日平安顺遂",
-icon:"🍀"
+desc:"今日平安顺遂"
 }
 
 ];
 
 
-// 随机抽取
+
 function getFortune(){
 
-let random=Math.random()*100;
+let r=Math.random()*100;
 
 
-if(random<5){
+if(r<5){
 
 return fortunes[0];
 
 }
 
-else if(random<20){
+else if(r<20){
 
 return fortunes[1];
 
 }
 
-else if(random<50){
+else if(r<50){
 
 return fortunes[2];
 
 }
 
-else if(random<80){
+else if(r<80){
 
 return fortunes[3];
 
@@ -78,7 +78,5 @@ else{
 return fortunes[4];
 
 }
-
-
 
 }
